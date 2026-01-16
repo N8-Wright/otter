abaa70a23f94d1f96486cea7639c996881cd6168
Nathaniel Wright <nathan@nathanieljwright.com>
2026-01-15 17:47:50 -0800

    Added more tests

--- otter_cstring_tests.c
+++ otter_cstring_tests.c
@@ -16,6 +16,8 @@
  */
 #include "otter_cstring.h"
 #include "otter_test.h"
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <string.h>
 OTTER_TEST(strdup_works) {
@@ -100,4 +102,129 @@ OTTER_TEST(strndup_allocator_null) {
 
   OTTER_ASSERT(dup == NULL);
   OTTER_TEST_END();
+}
+
+OTTER_TEST(strndup_partial_copy) {
+  const char *str = "Hello World";
+  char *dup = otter_strndup(OTTER_TEST_ALLOCATOR, str, 5);
+
+  OTTER_ASSERT(dup != NULL);
+  OTTER_ASSERT(strcmp(dup, "Hello") == 0);
+  OTTER_ASSERT(strlen(dup) == 5);
+
+  OTTER_TEST_END(otter_free(OTTER_TEST_ALLOCATOR, dup););
+}
+
+OTTER_TEST(vasprintf_allocator_null) {
+  char *str = NULL;
+  va_list args;
+  bool result = otter_vasprintf(NULL, &str, "test", args);
+
+  OTTER_ASSERT(result == false);
+  OTTER_ASSERT(str == NULL);
+  OTTER_TEST_END();
+}
+
+OTTER_TEST(vasprintf_str_null) {
+  va_list args;
+  bool result = otter_vasprintf(OTTER_TEST_ALLOCATOR, NULL, "test", args);
+
+  OTTER_ASSERT(result == false);
+  OTTER_TEST_END();
+}
+
+OTTER_TEST(vasprintf_fmt_null) {
+  char *str = NULL;
+  va_list args;
+  bool result = otter_vasprintf(OTTER_TEST_ALLOCATOR, &str, NULL, args);
+
+  OTTER_ASSERT(result == false);
+  OTTER_ASSERT(str == NULL);
+  OTTER_TEST_END();
+}
+
+static bool vasprintf_helper(otter_allocator *allocator, char **str,
+                             const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  bool result = otter_vasprintf(allocator, str, fmt, args);
+  va_end(args);
+  return result;
+}
+
+OTTER_TEST(vasprintf_works) {
+  char *str = NULL;
+  bool result =
+      vasprintf_helper(OTTER_TEST_ALLOCATOR, &str, "Hello %s %d", "World", 42);
+
+  OTTER_ASSERT(result == true);
+  OTTER_ASSERT(str != NULL);
+  OTTER_ASSERT(strcmp(str, "Hello World 42") == 0);
+
+  OTTER_TEST_END(otter_free(OTTER_TEST_ALLOCATOR, str););
+}
+
+OTTER_TEST(vasprintf_malloc_fails) {
+  otter_allocator_vtable vtable = {
+      .malloc = malloc_mock,
+      .realloc = NULL,
+      .free = NULL,
+  };
+  otter_allocator allocator = {
+      .vtable = &vtable,
+  };
+
+  char *str = NULL;
+  bool result = vasprintf_helper(&allocator, &str, "test %d", 123);
+
+  OTTER_ASSERT(result == false);
+  OTTER_ASSERT(str == NULL);
+  OTTER_TEST_END();
+}
+
+OTTER_TEST(asprintf_allocator_null) {
+  char *str = NULL;
+  bool result = otter_asprintf(NULL, &str, "test");
+
+  OTTER_ASSERT(result == false);
+  OTTER_ASSERT(str == NULL);
+  OTTER_TEST_END();
+}
+
+OTTER_TEST(asprintf_str_null) {
+  bool result = otter_asprintf(OTTER_TEST_ALLOCATOR, NULL, "test");
+
+  OTTER_ASSERT(result == false);
+  OTTER_TEST_END();
+}
+
+OTTER_TEST(asprintf_fmt_null) {
+  char *str = NULL;
+  bool result = otter_asprintf(OTTER_TEST_ALLOCATOR, &str, NULL);
+
+  OTTER_ASSERT(result == false);
+  OTTER_ASSERT(str == NULL);
+  OTTER_TEST_END();
+}
+
+OTTER_TEST(asprintf_works) {
+  char *str = NULL;
+  bool result = otter_asprintf(OTTER_TEST_ALLOCATOR, &str, "Number: %d", 999);
+
+  OTTER_ASSERT(result == true);
+  OTTER_ASSERT(str != NULL);
+  OTTER_ASSERT(strcmp(str, "Number: 999") == 0);
+
+  OTTER_TEST_END(otter_free(OTTER_TEST_ALLOCATOR, str););
+}
+
+OTTER_TEST(asprintf_empty_format) {
+  char *str = NULL;
+  bool result = otter_asprintf(OTTER_TEST_ALLOCATOR, &str, "");
+
+  OTTER_ASSERT(result == true);
+  OTTER_ASSERT(str != NULL);
+  OTTER_ASSERT(strcmp(str, "") == 0);
+
+  OTTER_TEST_END(otter_free(OTTER_TEST_ALLOCATOR, str););
 }
